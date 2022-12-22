b92412d248d16a89f2bc15bc4fefc9c97d1e5c40
eglauko <eglauko@hotmail.com>
2022-12-22 19:01:52 -0300

    removendo get projection de async list result porque não faz sentido.

--- RoyalCode.EnterprisePatterns/RoyalCode.Searches.Abstractions/IAsyncResultList.cs
+++ RoyalCode.EnterprisePatterns/RoyalCode.Searches.Abstractions/IAsyncResultList.cs
@@ -10,14 +10,4 @@ public interface IAsyncResultList<out TModel> : IResultList
     /// Async collection of the searched models.
     /// </summary>
     IAsyncEnumerable<TModel> Items { get; }
-    
-    /// <summary>
-    /// Gets a value from the projection if it exists and is of the type entered,
-    /// or returns the default value if the value does not exist or the type is different.
-    /// </summary>
-    /// <typeparam name="T">Projection value type.</typeparam>
-    /// <param name="name">Projection name.</param>
-    /// <param name="defaultValue">Default value.</param>
-    /// <returns>The projection value, or default value.</returns>
-    T GetProjection<T>(string name, T? defaultValue = default);
 }
